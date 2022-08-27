ab1812452b0b55983da7359e04c8df4793769baa
AllJavi <j.madrigal.torija@gmail.com>
2022-08-27 18:47:40 +0200

    build remove

--- build/CMakeFiles/org.kde.plasma.virtualdesktopbar-plasmoids-metadata-json.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for org.kde.plasma.virtualdesktopbar-plasmoids-metadata-json.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alljavi/virtual-desktop-bar
-# Build directory: /home/alljavi/virtual-desktop-bar/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(appstreamtest "/usr/bin/cmake" "-DAPPSTREAMCLI=/usr/bin/appstreamcli" "-DINSTALL_FILES=/home/alljavi/virtual-desktop-bar/build/install_manifest.txt" "-P" "/usr/share/ECM/kde-modules/appstreamtest.cmake")
-set_tests_properties(appstreamtest PROPERTIES  _BACKTRACE_TRIPLES "/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;162;add_test;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;180;appstreamtest;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;0;;/home/alljavi/virtual-desktop-bar/CMakeLists.txt;38;include;/home/alljavi/virtual-desktop-bar/CMakeLists.txt;0;")
